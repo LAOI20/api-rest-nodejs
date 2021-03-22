4cc59eb7fd1391eb00f0a841980ac8e9bc837413
magicApss <68205222+magicApss@users.noreply.github.com>
2021-03-22 16:38:02 -0600

    first deploy

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var express = require('express');
+var app = express();
+
+const stripe = require('stripe')('sk_test_51IXow6BLs2QnbtAB2BCcinTcYmtTw1sJnIWH4ZSy87B1xmxiSkmwa0z3AtgXnICWcm6EgytDFDsnVAS8Z8cti0yz00Y2txJLBI');
+
+
+var port = process.env.PORT || 8080;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.post('/paymentIntent', async (req, res) => {
+
+    try {
+
+        var tokenCard = await stripe.tokens.create({
+            card: {
+                number: req.body.cardNumber,
+                exp_month: req.body.expMonth,
+                exp_year: req.body.expYear,
+                cvc: req.body.cvc,
+            }
+        });
+    
+        var resultt = await stripe.charges.create({
+            amount: 15000,
+            currency: 'mxn',
+            source: tokenCard.id
+        }); 
+        
+        res.send(resultt);
+        
+    } catch (ee) {        
+        console.log("ERRRRORRR " + ee.type);
+        console.log("ERRRRORRR " + ee);
+
+        res.send('error');
+    }
+});
+
+
+app.listen(port, () => {
+    console.log('SERVER RUNNING ON PORT ' + port);
+});
